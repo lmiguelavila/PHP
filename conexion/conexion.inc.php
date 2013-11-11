856b377a31908b56c868b6ca29b4ac59c7747219
Luis Miguel <lmiguelavila@gmail.com>
2013-11-11 13:37:36 -0500

    Clase conexión MySQLi
    Clase que utiliza MySQLi para realizar la conexión a la base de datos

---
+++ conexion/conexion.inc.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once ('config.inc.php');
+
+class db{
+
+	private $conn;
+
+	public function conexion(){
+
+		$this->conn = mysqli_connect(dbhost, dbuser, dbpass, dbname);
+		if (mysqli_connect_errno()) {
+			die('Error de conexi&oacute;n MySQLi');
+		}
+
+	}
+
+	public function consulta($query){
+		
+		$resultado = mysqli_query($this->conn,$query);
+		if (!$resultado) {
+			die('Error al realizar la consulta');
+		}
+
+		return $resultado;
+	}
+
+	public function id(){
+		return mysqli_insert_id($this->conn);
+	}
+
+	public function escape($value){
+		return mysqli_real_escape_string($this->conn,$value);
+	}
+
+	public function liberar(){
+		mysqli_free_result($this->conn);
+	}
+
+	public function cerrar(){
+		mysqli_close($this->conn);
+	}
+
+}
+
+?>
---
+++ conexion/config.inc.php
@@ -0,0 +1,8 @@
+<?php 
+
+define ('dbhost' , 'host');
+define ('dbuser' , 'user');
+define ('dbpass' , 'pass');
+define ('dbname' , 'db');
+
+?>
